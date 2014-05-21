aa39882b9a9cd982b24968fc9a0ac1ff37bfacf8
Marcos Lopez C <MarcosLopezC@live.com>
2014-05-21 19:21:54 -0400

    Adding List object.

--- Sources/js/capsule.js
+++ Sources/js/capsule.js
@@ -555,5 +555,121 @@ var Capsule = (function() {
 		return Stack;
 	}());
 
+	Capsule.List = (function() {
+		var List = function() {
+			Object.defineProperties(this, {
+				_items: {
+					value: [],
+					writable: true
+				}
+			});
+		};
+
+		var defineProperty = function(key, accessor) {
+			Object.defineProperty(List.prototype, key, {
+				enumerable: true,
+				get: accessor.get,
+				set: accessor.set
+			});
+		};
+
+		defineProperty("count", {
+			get: function() {
+				return this._items.length;
+			}
+		});
+
+		List.prototype.add = function(item) {
+			this._items.push(item);
+			return this;
+		};
+
+		List.prototype.insert = function(index, item) {
+			var items = this._items;
+			if (index > items.count - 1) {
+				this.add(item);
+			}
+			items.splice(index, 0, item);
+			return this;
+		};
+
+		List.prototype.getItemAt = function(index) {
+			return this._items[index];
+		};
+
+		List.prototype.getIndexOf = function(item) {
+			return this._items.indexOf(item);
+		};
+
+		List.prototype.getFirst = function() {
+			return this._items[0];
+		};
+
+		List.prototype.getLast = function() {
+			var items = this._items;
+			return items[items.length - 1];
+		};
+
+		List.prototype.find = function(predicate) {
+			var i;
+			var count = this.count;
+			var items = this._items;
+			for (i = 0; i < count; i += 1) {
+				if (predicate(items[i])) {
+					return items[i];
+				}
+			}
+		};
+
+		List.prototype.findAll = function(predicate) {
+			var i;
+			var results = [];
+			var count = this.count;
+			var items = this._items;
+			for (i = 0; i < count; i += 1) {
+				if (predicate(items[i])) {
+					results.push(items[i]);
+				}
+			}
+			return results;
+		};
+
+		List.prototype.reverse = function() {
+			this._items.reverse();
+			return this;
+		};
+
+		List.prototype.clear = function() {
+			this._items = [];
+			return this;
+		};
+
+		List.prototype.removeAt = function(index) {
+			var item = this._items;
+			if (index >= 0) {
+				item.splice(index, 1);
+			}
+			return this;
+		};
+
+		List.prototype.remove = function(item) {
+			this.removeAt(this._items.indexOf(item));
+			return this;
+		};
+
+		List.prototype.clone = function() {
+			var list = new List();
+			list._items = this._items.slice(0);
+			return list;
+		};
+
+		List.prototype.forEach = function(lambda) {
+			this._items.forEach(lambda);
+			return this;
+		};
+
+		return List;
+	}());
+
 	return Capsule;
 }());
