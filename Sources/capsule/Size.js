253c8a62744cb111882206fe887102443281e288
Marcos Lopez C <MarcosLopezC@live.com>
2014-06-07 19:26:10 -0400

    Adding width and height accessors to prevent negative values.

--- Sources/capsule/Size.js
+++ Sources/capsule/Size.js
@@ -1,12 +1,37 @@
 // Requires: capsule.js
+// Requires: utilities.js
 
 capsule.Size = (function() {
 	"use strict";
 
 	var Size = function(width, height) {
-		this.width  = width  || 0;
-		this.height = height || 0;
+		this._width  = width  || 0;
+		this._height = height || 0;
+
+		capsule.utilities.applyDataDescriptor(this);
 	};
+
+	Size.prototype.width  = null;
+	Size.prototype.height = null;
+
+	capsule.utilities.defineAccessorProperties(Size.prototype, {
+		width: {
+			get: function() {
+				return this._width;
+			},
+			set: function(value) {
+				this._width = Math.max(0, value);
+			}
+		},
+		height: {
+			get: function() {
+				return this._height;
+			},
+			set: function(value) {
+				this._height = Math.max(0, value);
+			}
+		}
+	});
 
 	Size.prototype.clone = function() {
 		return new Size(this.width, this.height);
