18db2a3a48b581b0cce0d6a1fe399b003bfaa0e6
Marcos Lopez C <MarcosLopezC@live.com>
2014-06-18 07:31:25 -0400

    Refactoring utilities object and fixing bugs.

--- Sources/capsule/utilities.js
+++ Sources/capsule/utilities.js
@@ -1,55 +1,54 @@
 // Requires: capsule.js
 
-capsule.utilities = Object.create(null);
-
-capsule.utilities.applyDataDescriptor = (function() {
+capsule.utilities = (function() {
 	"use strict";
 
-	var isConstant = function(name) {
-		return name.toLocaleUpperCase() === name;
+	var utilities = Object.create(null);
+
+	// Returns a value indicating whether the given name
+	// beings to a writable member.
+	var isWritable = function(name) {
+		// True if the name is not all upper case.
+		return name.toLocaleUpperCase() !== name;
 	};
 
-	var isPrivate = function(name) {
-		return name.charAt(0) === "_";
+	// Returns a value indicating whether the given name
+	// belongs to an enumerable member.
+	var isEnumerable = function(name) {
+		// True if the name does not start with an underscore.
+		return name.charAt(0) !== "_";
 	};
 
-	return function(object) {
-		var descriptor = {
-			writable:     true,
-			configurable: true,
-			enumerable:   true
-		};
-
-		Object.getOwnPropertyNames(object).forEach(function(key) {
-			if (isConstant(key)) {
-				descriptor.writable = false;
-			}
-
-			if (isPrivate(key)) {
-				descriptor.enumerable = false;
-			}
-
-			Object.defineProperty(object, key, descriptor);
-		});
-	};
-}());
-
-capsule.utilities.defineAccessorProperties = function(object, accessors) {
-	"use strict";
-
+	// The accessor function used when none is provided.
 	var defaultAccessor = function() {
 		throw new Error("Accessor is not defined.");
 	};
 
-	Object.getOwnPropertyNames(accessors).forEach(function(key) {
-		if (typeof accessors[key].get !== "function") {
-			throw new Error("Accessor descriptors must have a 'get' property.");
-		}
-
-		Object.defineProperty(object, key, {
-			enumerable: true,
-			get:        accessors[key].get,
-			set:        accessors[key].set || defaultAccessor
+	// Iterates through each key in the given object and applies
+	// data descriptors based on the key's name.
+	utilities.applyDataDescriptor = function(object) {
+		Object.getOwnPropertyNames(object).forEach(function(key) {
+			Object.defineProperty(object, key, {
+				writable:   isWritable(key),
+				enumerable: isEnumerable(key)
+			});
 		});
-	});
-};
+	};
+
+	// Defines the accessor properties for the given object.
+	utilities.defineAccessorProperties = function(object, accessors) {
+		Object.getOwnPropertyNames(accessors).forEach(function(key) {
+			if (typeof accessors[key].get !== "function") {
+				throw new Error("Accessor descriptors must have a 'get' function.");
+			}
+
+			Object.defineProperty(object, key, {
+				enumerable: true,
+				get:        accessors[key].get,
+				set:        accessors[key].set || defaultAccessor
+			});
+		});
+	};
+
+	return utilities;
+}());
