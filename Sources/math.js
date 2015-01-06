700c2f79c2d4b43725e7e5df3dc740401e9d08ef
Marcos Lopez C <MarcosLopezC@live.com>
2015-01-06 00:49:26 -0500

    Removing aliases to avoid future linking issues.

--- Sources/math.js
+++ Sources/math.js
@@ -35,20 +35,16 @@ exports.normalize = function(value, min, max) {
 	return (value - min) / (max - min);
 };
 
-var normalize = exports.normalize;
-
 // Returns the linear interpolation of the given normalized value over the range [min, max).
 exports.linearInterpolation = function(value, min, max) {
 	return (1 - value) * min + value * max;
 };
 
-var linearInterpolation = exports.linearInterpolation;
-
 // Returns a value in the range [toMin, toMax) resulting from mapping the given value
 // in the range [fromMin, fromMax).
 exports.map = function(value, fromMin, fromMax, toMin, toMax) {
-	var normal = normalize(value, fromMin, fromMax);
-	return linearInterpolation(normal, toMin, toMax);
+	var normal = exports.normalize(value, fromMin, fromMax);
+	return exports.linearInterpolation(normal, toMin, toMax);
 };
 
 // Returns the value of a random angle, in radians.
@@ -58,14 +54,12 @@ exports.randomAngle = function() {
 
 // Returns the value of a random number in the range [min, max).
 exports.randomNumber = function(min, max) {
-	return linearInterpolation(Math.random(), min, max);
+	return exports.linearInterpolation(Math.random(), min, max);
 };
-
-var randomNumber = exports.randomNumber;
 
 // Returns the value of a random integer in the range [min, max].
 exports.randomInteger = function(min, max) {
-	return randomNumber(min, max + 1) | 0;
+	return exports.randomNumber(min, max + 1) | 0;
 };
 
 // Returns the given radian angle represented in degrees.
@@ -73,47 +67,43 @@ exports.toDegrees = function(radians) {
 	return (radians * 180) / Math.PI;
 };
 
-var toDeg = exports.toDegrees;
-
 // Returns the given degree angle represented in radians.
 exports.toRadians = function(degrees) {
 	return (degrees * Math.PI) / 180;
 };
 
-var toRad = exports.toRadians;
-
 // Returns the sine of an angle in degrees.
 exports.sin = function(x) {
-	return Math.sin(toRad(x));
+	return Math.sin(exports.toRadians(x));
 };
 
 // Returns the cosine of an angle in degrees.
 exports.cos = function(x) {
-	return Math.cos(toRad(x));
+	return Math.cos(exports.toRadians(x));
 };
 
 // Returns the tangent of an angle in degrees.
 exports.tan = function(x) {
-	return Math.tan(toRad(x));
+	return Math.tan(exports.toRadians(x));
 };
 
 // Returns the arcSin of a value, in degrees.
 exports.arcSin = function(x) {
-	return toDeg(Math.aSin(x));
+	return exports.toDegrees(Math.aSin(x));
 };
 
 // Returns the arcCos of a value, in degrees.
 exports.arcCos = function(x) {
-	return toDeg(Math.aCos(x));
+	return exports.toDegrees(Math.aCos(x));
 };
 
 // Returns the arcTan of a value, in degrees.
 exports.arcTan = function(y, x) {
 	if (typeof x === "number") {
-		return toDeg(Math.atan2(y, x));
+		return exports.toDegrees(Math.atan2(y, x));
 	}
 	else {
-		return toDeg(Math.atan(y));
+		return exports.toDegrees(Math.atan(y));
 	}
 };
 
