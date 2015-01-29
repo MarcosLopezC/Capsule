10bc0e2adb73fd7a439a47e8615b01a479f8e64f
Marcos Lopez C <MarcosLopezC@live.com>
2015-01-29 17:03:25 -0500

    Adding comments.

--- Sources/Style.js
+++ Sources/Style.js
@@ -6,12 +6,14 @@
 
 var Color = require("./Color.js");
 
+// Initializes a new instance of the Style class.
 var Style = module.exports = function() {
 	this.fillColor   = new Color();
 	this.strokeColor = new Color();
 	this.strokeThickness = 1;
 };
 
+// Returns a deep copy of this style.
 Style.prototype.clone = function() {
 	var style = new Style();
 	style.fillColor   = this.fillColor.clone();
@@ -20,6 +22,7 @@ Style.prototype.clone = function() {
 	return style;
 };
 
+// Applies this style to the given 2D canvas context.
 Style.prototype.apply = function(context) {
 	context.fillStyle   = this.fillColor.toString();
 	context.strokeStyle = this.strokeColor.toString();
